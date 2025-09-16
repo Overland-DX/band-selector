338c64a959d3e5300891c1beca26e7fc8dbc2f45
Jan Ove Overland <102610897+Overland-DX@users.noreply.github.com>
2025-09-16 23:35:28 +0200

    Update BandSelector.js v2.0

--- BandSelector.js
+++ BandSelector.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Band Selector',
-    version: '1.5',
+    version: '2.0',
     author: 'ODX',
     frontEndPath: 'BandSelector/frontend.js'
 }
@@ -12,3 +12,4 @@ var pluginConfig = {
 module.exports = {
     pluginConfig
 }
+
